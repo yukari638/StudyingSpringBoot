6433e51992f8816e4698ca29d9a55987d1210cb0
yukari638 <yukari@localhost>
2021-01-09 02:24:31 +0900

    Postについてのsampleコードを追加。ControllerのGetMapping("/")の内容も合わせて変更。

--- src/main/java/com/example/demo/contller/HelloController.java
+++ src/main/java/com/example/demo/contller/HelloController.java
@@ -3,15 +3,16 @@ package com.example.demo.contller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
 	@GetMapping("/")
-	public String greeting() {
+	public String index() {
 		
-		return "hello";
+		return "form";
 	}
 	
 	@GetMapping("/greeting")
@@ -19,5 +20,11 @@ public class HelloController {
 		
 		model.addAttribute("sample", message);
 		return "hello";
+	}
+	
+	@PostMapping("/confirm")
+	public String comfirm(@RequestParam String message, Model model) {
+		model.addAttribute("message", message);
+		return "confirm";
 	}
 }
